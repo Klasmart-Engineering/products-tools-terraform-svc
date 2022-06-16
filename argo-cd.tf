f21a7e65f2c8f40fa80b5954f0ad2b5ce8586431
andreas.marneris <andreas.marneris@kidsloop.live>
2022-06-16 11:39:07 +0100

    feat: made argocd apps into a template triggered via a named list

--- argo-cd.tf
+++ argo-cd.tf
@@ -1,6 +1,7 @@
-resource "argocd_application" "microgateway" {
+resource "argocd_application" "applications" {
+  for_each = toset(var.argocd_app_names)
   metadata {
-    name      = "microgateway-${local.name_suffix}"
+    name      = "${each.key}-${local.name_suffix}"
     namespace = var.argocd_namespace
     labels = {
       region = var.region
@@ -16,7 +17,7 @@ resource "argocd_application" "microgateway" {
     project = var.argocd_project
     source {
       repo_url        = var.helm_chart_url
-      path            = "${local.project_environment}/${local.project_region}/manifests/microgateway/"
+      path            = "${local.project_environment}/${local.project_region}/manifests/${each.key}/"
       target_revision = var.microgateway_helm_chart_revision
       helm {
         value_files = ["values.yaml"]
@@ -27,53 +28,6 @@ resource "argocd_application" "microgateway" {
       server    = var.kubernetes_server_url
       namespace = var.product_namespace
     }
-    sync_policy {
-      automated = {
-        prune       = true
-        self_heal   = true
-        allow_empty = false
-      }
-      retry {
-        backoff = {
-          duration     = ""
-          max_duration = ""
-        }
-        limit = "0"
-      }
-    }
-  }
-}
-
-resource "argocd_application" "istio" {
-  metadata {
-    name      = "istio-config-${local.name_suffix}"
-    namespace = var.argocd_namespace
-    labels = {
-      region = var.region
-      env    = var.project_environment
-      loc    = var.project_region
-      owner  = var.service_owner
-      layer  = "product"
-    }
-  }
-  wait = true
-
-  spec {
-    project = var.argocd_project
-    source {
-      repo_url        = var.helm_chart_url
-      path            = "${local.project_environment}/${local.project_region}/manifests/istio"
-      target_revision = var.istio_helm_chart_revision
-      helm {
-        value_files = ["values.yaml"]
-      }
-    }
-
-    destination {
-      server    = var.kubernetes_server_url
-      namespace = var.product_namespace
-    }
-
     sync_policy {
       automated = {
         prune       = true
